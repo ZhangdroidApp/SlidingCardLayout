1c356f03bc0b2b96cbd98ea831dd80b96e9f0111
zhangdroid <zhangliangspring@126.com>
2016-10-27 20:10:25 +0800

    SlidingCardLayout 增加堆叠效果

--- library/src/main/java/com/zhangdroid/library/widgets/SlidingCardLayout.java
+++ library/src/main/java/com/zhangdroid/library/widgets/SlidingCardLayout.java
@@ -3,19 +3,23 @@ package com.zhangdroid.library.widgets;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.database.DataSetObserver;
 import android.graphics.Point;
+import android.support.v4.view.MotionEventCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.widget.ViewDragHelper;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.animation.AccelerateInterpolator;
-import android.view.animation.DecelerateInterpolator;
+import android.view.animation.LinearInterpolator;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 
+import com.zhangdroid.library.R;
 import com.zhangdroid.library.utils.DensityUtil;
 import com.zhangdroid.library.utils.LogUtil;
 
@@ -28,10 +32,7 @@ import java.util.List;
  */
 public class SlidingCardLayout extends FrameLayout {
     private static final String TAG = "SlidingCardLayout";
-    /**
-     * 默认可见的子view个数
-     */
-    private static final int DEFAULT_VISIBLE_CHILDVIEW_COUNT = 3;
+
     /**
      * 默认动画时长
      */
@@ -39,20 +40,28 @@ public class SlidingCardLayout extends FrameLayout {
     /**
      * 默认动画透明度
      */
-    private static final float DEFAULT_ANIMATOR_ALPHA = 0.7f;
+    private static final float DEFAULT_ANIMATOR_ALPHA = 0.75f;
     /**
      * 默认动画旋转角度
      */
-    private static final int DEFAULT_ANIMATOR_ROTATION = 45;
+    private static final int DEFAULT_ANIMATOR_ROTATION = 30;
+    /**
+     * 默认可见的子view个数
+     */
+    private static final int DEFAULT_VISIBLE_CHILD_VIEW_COUNT = 3;
+    /**
+     * 默认y轴方向上的偏移量步长
+     */
+    private static final int DEFAULT_Y_OFFSET_STEP = 20;
+    /**
+     * 默认缩放步长
+     */
+    private static final float DEFAULT_SCALE_STEP = 0.06f;
 
     /**
      * X方向上滑动的阙值
      */
     private int x_distance_threhold;
-    /**
-     * 滑动的阙值
-     */
-    private int mTouchSlop;
     /**
      * 判定为抛飞动作的最小速度
      */
@@ -79,13 +88,14 @@ public class SlidingCardLayout extends FrameLayout {
      */
     private float[] mOriginalCenter = new float[2];
     /**
-     * 保存释放后的view列表(包括滑出方向)
+     * 保存释放后的view列表
      */
     private List<View> mReleasedViewList = new ArrayList<View>();
     /**
-     * 保存释放后的view对应的数据的列表
+     * 保存释放后的view对应的数据列表
      */
-    private List<Object> mRelasedViewDataList = new ArrayList<Object>();
+    private List<Object> mReleasedViewDataList = new ArrayList<Object>();
+
     private boolean mIsReverting = false;
     private boolean mIsFlingLeft = false;
     private boolean mIsFlingRight = false;
@@ -95,13 +105,13 @@ public class SlidingCardLayout extends FrameLayout {
      */
     private int mVisibleViewCount;
     /**
-     * adapter中缓冲保存子view栈的大小
+     * y轴方向上偏移量步长
      */
-    private int mAdapterStackSize;
+    private int yOffsetStep;
     /**
-     * 是否显示堆叠的结构
+     * view的缩放步长
      */
-    private boolean mIsStacked;
+    private float scaleStep;
 
     private OnCardSlidingListener mOnCardSlidingListener;
     private OnCardItemClickListener mOnCardItemClickListener;
@@ -111,19 +121,23 @@ public class SlidingCardLayout extends FrameLayout {
         @Override
         public void onChanged() {
             super.onChanged();
+            LogUtil.e(TAG, "onChanged()");
+            removeAllViewsInLayout();
+            mTopCardView = null;
+            attachChildViews();
             requestLayout();
         }
 
         @Override
         public void onInvalidated() {
             super.onInvalidated();
-            removeAllViews();
+            removeAllViewsInLayout();
             mTopCardView = null;
             if (mReleasedViewList != null) {
                 mReleasedViewList.clear();
             }
-            if (mRelasedViewDataList != null) {
-                mRelasedViewDataList.clear();
+            if (mReleasedViewDataList != null) {
+                mReleasedViewDataList.clear();
             }
         }
     };
@@ -138,26 +152,36 @@ public class SlidingCardLayout extends FrameLayout {
 
     public SlidingCardLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init(context);
-        mAdapterStackSize = 2 * DEFAULT_VISIBLE_CHILDVIEW_COUNT;
 
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.SlidingCardLayout);
+        mVisibleViewCount = typedArray.getInteger(R.styleable.SlidingCardLayout_visibleCount, DEFAULT_VISIBLE_CHILD_VIEW_COUNT);
+        yOffsetStep = typedArray.getDimensionPixelSize(R.styleable.SlidingCardLayout_yOffsetStep,
+                (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, DEFAULT_Y_OFFSET_STEP, getResources().getDisplayMetrics()));
+        scaleStep = typedArray.getFloat(R.styleable.SlidingCardLayout_scaleStep, DEFAULT_SCALE_STEP);
+        typedArray.recycle();
+
+        init(context);
     }
 
     private void init(Context context) {
-        mViewDragHelper = ViewDragHelper.create(this, 1.0f, mVDHCallback);
+        mViewDragHelper = ViewDragHelper.create(this, 10.0f, mVDHCallback);
 
         ViewConfiguration viewConfiguration = ViewConfiguration.get(context);
-        mTouchSlop = viewConfiguration.getScaledTouchSlop();
-        x_distance_threhold = 5 * mTouchSlop;
+        x_distance_threhold = 2 * viewConfiguration.getScaledTouchSlop();
         mMinFlingVelocity = viewConfiguration.getScaledMinimumFlingVelocity();
         mScreenWidth = DensityUtil.getScreenWidth(context);
 
-        LogUtil.e(TAG, "init() touchSlop = " + mTouchSlop + "  flingVelocity = " + mMinFlingVelocity
+        LogUtil.e(TAG, "init() x_distance_threhold = " + x_distance_threhold + "  flingVelocity = " + mMinFlingVelocity
                 + "  mScreenWidth = " + mScreenWidth);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mViewDragHelper.cancel();
+            return false;
+        }
         return mViewDragHelper.shouldInterceptTouchEvent(ev);
     }
 
@@ -178,27 +202,33 @@ public class SlidingCardLayout extends FrameLayout {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         if (mBaseAdapter != null) {
             int adapterCount = mBaseAdapter.getCount();
-            LogUtil.e(TAG, "onLayout() mAdapterStackSize = " + mAdapterStackSize + "  adapterCount = " + adapterCount);
+            LogUtil.e(TAG, "onLayout() mVisibleViewCount = " + mVisibleViewCount + "  adapterCount = " + adapterCount);
             if (adapterCount > 0) {
                 // 计算当前需要添加的子view的个数
-                int child_view_stack = Math.min(mAdapterStackSize, adapterCount);
-                // 所有的子view按照从上到下的顺序堆叠放置
+                int child_view_stack = Math.min(mVisibleViewCount, adapterCount);
                 for (int pos = 0; pos < child_view_stack; pos++) {
                     View childView = getChildAt(pos);
                     if (childView != null) {
-                        int left = getPaddingLeft();
+                        int left = getPaddingLeft() + (getWidth() - childView.getMeasuredWidth()) / 2;
                         int top = getPaddingTop();
                         childView.layout(left, top, left + childView.getMeasuredWidth(), top + childView.getMeasuredHeight());
+                        // 设置堆叠效果
+                        int step = child_view_stack - pos - 1;
+                        float scale = 1 - step * scaleStep;
+                        int offset = step * yOffsetStep;
+                        LogUtil.e(TAG, "pos = " + pos + " step = " + step + " scale =" + scale + " offset =" + offset);
+                        childView.setScaleX(scale);
+                        childView.setScaleY(scale);
+                        childView.offsetTopAndBottom(offset);
                     }
                 }
                 // 设置当前顶部卡片view
                 setupTopCardView();
+                if (adapterCount <= mVisibleViewCount && mOnCardSlidingListener != null) {
+                    mOnCardSlidingListener.onAdapterApproachInEmpty(adapterCount);
+                }
             } else {
                 removeAllViewsInLayout();
-            }
-
-            if (adapterCount <= mAdapterStackSize && mOnCardSlidingListener != null) {
-                mOnCardSlidingListener.onAdapterApproachInEmpty(adapterCount);
             }
         }
     }
@@ -241,6 +271,7 @@ public class SlidingCardLayout extends FrameLayout {
 
         @Override
         public boolean tryCaptureView(View child, int pointerId) {
+            LogUtil.e(TAG, "tryCaptureView() " + (child == mTopCardView));
             return child == mTopCardView;
         }
 
@@ -281,12 +312,12 @@ public class SlidingCardLayout extends FrameLayout {
 
         @Override
         public int getViewVerticalDragRange(View child) {
-            return child == mTopCardView ? getMeasuredHeight() - child.getMeasuredHeight() : 0;
+            return child == mTopCardView ? child.getMeasuredHeight() : 0;
         }
 
         @Override
         public int getViewHorizontalDragRange(View child) {
-            return child == mTopCardView ? getMeasuredWidth() - child.getMeasuredWidth() : 0;
+            return child == mTopCardView ? child.getMeasuredWidth() : 0;
         }
 
     };
@@ -313,45 +344,25 @@ public class SlidingCardLayout extends FrameLayout {
     }
 
     /**
-     * 移除当前顶部卡片view并从adapter中添加一个子view到末尾
-     *
-     * @param topCardView
+     * 移除当前顶部卡片view
      */
-    private void removeTopAndAddToLast(View topCardView) {
+    private void removeCurrTopCardView(View topCardView) {
         mReleasedViewList.add(topCardView);
-        mRelasedViewDataList.add(mBaseAdapter == null ? null : mBaseAdapter.getItem(0));
+        mReleasedViewDataList.add(getTopCardViewData());
         if (mOnRevertListener != null) {
             mOnRevertListener.onRevertStateChanged(true);
         }
         removeViewInLayout(topCardView);
-        addChildToLast();
-    }
-
-    /**
-     * 从adapter中添加一个view至FrameLayout的末尾(若adapter已没有待添加的view，则不做处理)
-     */
-    private void addChildToLast() {
-        if (mBaseAdapter != null) {
-            int adapterCount = mBaseAdapter.getCount();
-            int index = getChildCount();
-            if (adapterCount - index > 0) {// adapter中还有未添加过的子view
-                View childView = mBaseAdapter.getView(index + 1, null, this);
-                LogUtil.e(TAG, "addChildToLast() index = " + index + " childView is null = " + (childView == null));
-                if (childView != null) {
-                    FrameLayout.LayoutParams layoutParams = (LayoutParams) childView.getLayoutParams();
-                    addViewInLayout(childView, 0, layoutParams);
-                }
-            }
-        }
     }
 
     private void attachChildViews() {
         LogUtil.e(TAG, "attachChildViews()");
         if (mBaseAdapter != null) {
             int adapterCount = mBaseAdapter.getCount();
+            LogUtil.e(TAG, "attachChildViews() current adapter data count = " + adapterCount);
             if (adapterCount > 0) {
                 // 计算当前需要添加的子view的个数
-                int child_view_stack = Math.min(mAdapterStackSize, adapterCount);
+                int child_view_stack = Math.min(mVisibleViewCount, adapterCount);
                 // 所有的子view按照从上到下的顺序堆叠放置
                 for (int pos = child_view_stack - 1; pos >= 0; pos--) {
                     View childView = mBaseAdapter.getView(pos, null, this);
@@ -364,6 +375,7 @@ public class SlidingCardLayout extends FrameLayout {
         }
     }
 
+
     // *************************************** Public methods ***************************************
 
 
@@ -372,6 +384,13 @@ public class SlidingCardLayout extends FrameLayout {
      */
     public View getTopCardView() {
         return mTopCardView;
+    }
+
+    /**
+     * @return the data associated with the selected top card view
+     */
+    public Object getTopCardViewData() {
+        return mBaseAdapter == null ? null : mBaseAdapter.getItem(0);
     }
 
     /**
@@ -401,7 +420,7 @@ public class SlidingCardLayout extends FrameLayout {
             mTopCardView.animate()
                     .setDuration(duration)
                     .setInterpolator(new AccelerateInterpolator())
-                    .x(mTopCardView.getX() - mScreenWidth)
+                    .x(-1.5f * mScreenWidth)
                     .y(mTopCardView.getY())
                     .rotation(-DEFAULT_ANIMATOR_ROTATION)
                     .alpha(DEFAULT_ANIMATOR_ALPHA)
@@ -409,9 +428,9 @@ public class SlidingCardLayout extends FrameLayout {
 
                         @Override
                         public void onAnimationEnd(Animator animation) {
-                            removeTopAndAddToLast(mTopCardView);
+                            removeCurrTopCardView(mTopCardView);
                             if (mOnCardSlidingListener != null) {
-                                mOnCardSlidingListener.onLeftDisappear(mBaseAdapter == null ? null : mBaseAdapter.getItem(0));
+                                mOnCardSlidingListener.onLeftDisappear(getTopCardViewData());
                                 mOnCardSlidingListener.removeFirstObjInAdapter();
                                 mOnCardSlidingListener.onScroll(0.0f);
                             }
@@ -439,7 +458,7 @@ public class SlidingCardLayout extends FrameLayout {
             mTopCardView.animate()
                     .setDuration(duration)
                     .setInterpolator(new AccelerateInterpolator())
-                    .x(mTopCardView.getX() + mScreenWidth)
+                    .x(1.5f * mScreenWidth)
                     .y(mTopCardView.getY())
                     .rotation(DEFAULT_ANIMATOR_ROTATION)
                     .alpha(DEFAULT_ANIMATOR_ALPHA)
@@ -447,9 +466,9 @@ public class SlidingCardLayout extends FrameLayout {
 
                         @Override
                         public void onAnimationEnd(Animator animation) {
-                            removeTopAndAddToLast(mTopCardView);
+                            removeCurrTopCardView(mTopCardView);
                             if (mOnCardSlidingListener != null) {
-                                mOnCardSlidingListener.onRightDisappear(mBaseAdapter == null ? null : mBaseAdapter.getItem(0));
+                                mOnCardSlidingListener.onRightDisappear(getTopCardViewData());
                                 mOnCardSlidingListener.removeFirstObjInAdapter();
                                 mOnCardSlidingListener.onScroll(0.0f);
                             }
@@ -464,7 +483,7 @@ public class SlidingCardLayout extends FrameLayout {
      */
     public boolean canRevertToLast() {
         return (mReleasedViewList != null && mReleasedViewList.size() > 0)
-                && (mRelasedViewDataList != null && mRelasedViewDataList.size() > 0);
+                && (mReleasedViewDataList != null && mReleasedViewDataList.size() > 0);
     }
 
     /**
@@ -476,8 +495,8 @@ public class SlidingCardLayout extends FrameLayout {
             // 获得上一个卡片view
             final int indexOfViewList = mReleasedViewList.size() - 1;
             final View lastView = mReleasedViewList.get(indexOfViewList);
-            final int indexOfViewDataList = mRelasedViewDataList.size() - 1;
-            final Object lastViewData = mRelasedViewDataList.get(indexOfViewDataList);
+            final int indexOfViewDataList = mReleasedViewDataList.size() - 1;
+            final Object lastViewData = mReleasedViewDataList.get(indexOfViewDataList);
             if (lastView != null) {
                 // 恢复上一个被移除的卡片view
                 FrameLayout.LayoutParams layoutParams = (LayoutParams) lastView.getLayoutParams();
@@ -485,7 +504,7 @@ public class SlidingCardLayout extends FrameLayout {
                 // 将上一个被移除的卡片view移回到屏幕顶部
                 lastView.animate()
                         .setDuration(DEFAULT_ANIMATOR_DURATION)
-                        .setInterpolator(new DecelerateInterpolator())
+                        .setInterpolator(new LinearInterpolator())
                         .x(mOriginalCenter[0])
                         .y(mOriginalCenter[1])
                         .rotation(0)
@@ -496,7 +515,7 @@ public class SlidingCardLayout extends FrameLayout {
                             public void onAnimationEnd(Animator animation) {
                                 // 更新缓存数据列表
                                 mReleasedViewList.remove(indexOfViewList);
-                                mRelasedViewDataList.remove(indexOfViewDataList);
+                                mReleasedViewDataList.remove(indexOfViewDataList);
 
                                 // 将上一个被移除的卡片view的数据源添加到BaseAdapter中
                                 if (lastViewData != null && mOnRevertListener != null) {
@@ -547,7 +566,7 @@ public class SlidingCardLayout extends FrameLayout {
         /**
          * 更新adapter数据源，加载更多
          *
-         * @param adapterDataCount adapter中当前剩余的数据项个数
+         * @param adapterDataCount adapter中当前剩余的数据项数
          */
         void onAdapterApproachInEmpty(int adapterDataCount);
 
